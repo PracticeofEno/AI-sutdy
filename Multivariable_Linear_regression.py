c5317ce0923e5994ac73af1a303fe852d358f86e
haha <haha@gmail.com>
2023-02-13 22:49:28 +0900

    .

--- Multivariable_Linear_regression.py
+++ Multivariable_Linear_regression.py
@@ -16,3 +16,26 @@ w1 = torch.zeros(1, requires_grad=True)
 w2 = torch.zeros(1, requires_grad=True)
 w3 = torch.zeros(1, requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
+
+# optimizer 설정
+optimizer = optim.SGD([w1, w2, w3, b], lr=1e-5)
+
+nb_epochs = 1000
+for epoch in range(nb_epochs + 1):
+
+    # H(x) 계산
+    hypothesis = x1_train * w1 + x2_train * w2 + x3_train * w3 + b
+
+    # cost 계산
+    cost = torch.mean((hypothesis - y_train) ** 2)
+
+    # cost로 H(x) 개선
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    # 100번마다 로그 출력
+    if epoch % 100 == 0:
+        print('Epoch {:4d}/{} w1: {:.3f} w2: {:.3f} w3: {:.3f} b: {:.3f} Cost: {:.6f}'.format(
+            epoch, nb_epochs, w1.item(), w2.item(), w3.item(), b.item(), cost.item()
+        ))
